26dcd0e8db0c32c2845a563e54c3b827540e3cdd
TheSoftNode <thesoftnode@gmail.com>
2023-09-05 15:07:27 -0700

    Corrected code

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,28 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
+
 /**
- * check_cycle - check if linked list is cyclic
- * @list: the linked list
- * Return: 0 on False and 1 if true
+ * check_cycle - function in C that checks if a singly linked list has
+ *  a cycle in it
+ * @list: head of singly linked list
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *current = list;
+	listint_t *tmp;
 
-	if (current->next != NULL)
-	{
-		current = current->next;
-	}
-	else
-	{
+	if (!list)
 		return (0);
-	}
-	while (current != list)
+
+	while (list)
 	{
-		if (current->next == NULL)
-		{
-			return (0);
-		}
-		current = current->next;
+		tmp = list;
+		list = list->next;
+		if (tmp <= list)
+			return (1);
 	}
-	return (1);
+	return (0);
 }
