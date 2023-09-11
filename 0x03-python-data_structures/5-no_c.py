e6240c616ef5e92fbc6b679ec67cc9c09ba05d57
TheSoftNode <thesoftnode@gmail.com>
2023-09-11 14:26:14 -0700

    Added task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    slist = list(my_string)
-    [slist.remove(i) for i in slist if i == 'c' or i == 'C']
-    return("".join(slist))
+    """return string without c and C
+
+    Args:
+        my_string: the string
+
+    Returns:
+        a new string without c and C
+    """
+    new_string = ''
+    for i in range(len(my_string)):
+        if my_string[i] not in 'cC':
+            new_string += my_string[i]
+    return (new_string)
