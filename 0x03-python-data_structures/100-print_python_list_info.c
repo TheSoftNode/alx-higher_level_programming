899d64fe63143d2f5eb32fcd132413354ba3fae4
TheSoftNode <thesoftnode@gmail.com>
2023-09-13 13:22:12 -0700

    Modified codes

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
 #include <Python.h>
 
+void print_python_bytes(PyObject *p);
 /**
- * print_python_list_info - prints some basic info about Python lists
+ * print_python_list - prints some basic info about Python lists
  *
  * @p: Object of python
  *
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ * Return: No Return
  */
-void print_python_list_info(PyObject *p)
+void print_python_list(PyObject *p)
 {
-	long int size, alloc, i;
-        PyObject *item;
+	long int i, sizel, alloc;
+	PyObject *item;
 
-	size = PyList_Size(p);
+	sizel = PyList_Size(p);
 	alloc = ((PyListObject *)p)->allocated;
 
-	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Python list info\n");
+
+	printf("[*] Size of the Python List = %ld\n", sizel);
 	printf("[*] Allocated = %ld\n", alloc);
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < sizel; i++)
 	{
-		item = PyList_GetItem(p, i);
-		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
-
+		item = PyList_GET_ITEM(p, i);
+		printf("Element %ld: %s\n", i, ((item)->ob_type)->tp_name);
 	}
 }
