9501f98043239a9bf5a9c9f62d393d922fc485d1
TheSoftNode <thesoftnode@gmail.com>
2023-10-16 22:18:25 +0100

    Modified a file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-"""writing the base class"""
+
+""" Module of Base Class for Geometry Rectangle and Square """
+
 import json
 import csv
+import turtle
 
 
 class Base:
-    """writing the base class of this model"""
+    """ Class to define base model Object """
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """constructor method
-
-        Args:
-            id : the id
-        """
+        """ Constructor """
         if id is not None:
             self.id = id
         else:
@@ -23,82 +22,207 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """list of dictionary to json string"""
-        if list_dictionaries is None or list_dictionaries == []:
-            return ("[]")
-        else:
-            return (json.dumps(list_dictionaries))
+        """ returns the JSON string representation of list_dictionaries """
+        if not list_dictionaries:
+            return "[]"
+
+        return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """save the instance of the obj to file"""
+        """  writes the JSON string representation of list_objs to a file """
+        jlist = []
         filename = cls.__name__ + ".json"
-        with open(filename, "w") as myfile:
-            if (list_objs is None or list_objs == []):
-                myfile.write('[]')
-            else:
-                list_dict = [i.to_dictionary() for i in list_objs]
-                myfile.write(Base.to_json_string(list_dict))
+        if list_objs:
+            for i in list_objs:
+                jlist.append(i.to_dictionary())
+
+        st = cls.to_json_string(jlist)
+        with open(filename, "w", encoding="utf-8") as myfile:
+            myfile.write(st)
 
     @staticmethod
     def from_json_string(json_string):
-        """load list from json string"""
-        if json_string is None or json_string == "[]":
+        """ returns the list of the JSON string representation json_string """
+        if not json_string or len(json_string) == 0:
             return []
-        else:
-            return (json.loads(json_string))
+
+        return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
-        """create the class method"""
-        if dictionary and dictionary != {}:
-            if cls.__name__ == "Rectangle":
-                new = cls(1, 1)
-            else:
-                new = cls(1)
-            new.update(**dictionary)
-            return (new)
+        """ returns an instance with all attributes already set """
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
 
     @classmethod
     def load_from_file(cls):
-        """load from file"""
-        filename = str(cls.__name__) + ".json"
+        """ returns a list of instances from a file """
+        filename = cls.__name__ + ".json"
+
         try:
-            with open(filename, "r") as myfile:
-                list_dicts = Base.from_json_string(myfile.read())
-                return [cls.create(**d) for d in list_dicts]
+            with open(filename, encoding="utf-8") as myfile:
+                rd = myfile.read()
+                dicst = cls.from_json_string(rd)
+                inslist = []
+                for i in dicst:
+                    inslist.append(cls.create(**i))
+                return inslist
         except IOError:
             return []
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
-        """serialize from csv"""
+        """ Serializes to CSV and saves to file """
         filename = cls.__name__ + ".csv"
-        with open(filename, "w", newline="") as csvfile:
-            if list_objs is None or list_objs == []:
-                csvfile.write("[]")
-            else:
+        csvlist = []
+        if list_objs:
+            for i in list_objs:
+                dic = i.to_dictionary()
                 if cls.__name__ == "Rectangle":
-                    fieldnames = ["id", "width", "height", "x", "y"]
-                else:
-                    fieldnames = ["id", "size", "x", "y"]
-                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                for obj in list_objs:
-                    writer.writerow(obj.to_dictionary())
+                    csvlist.append([dic["id"], dic["width"],
+                                    dic["height"], dic["x"], dic["y"]])
+
+                elif cls.__name__ == "Square":
+                    csvlist.append([dic["id"], dic["size"],
+                                    dic["x"], dic["y"]])
+
+        with open(filename, "w", encoding="utf-8") as myfile:
+            w = csv.writer(myfile)
+            w.writerows(csvlist)
 
     @classmethod
     def load_from_file_csv(cls):
-        """load from the csv file"""
+        """ Deserializes from CSV and loads from file """
         filename = cls.__name__ + ".csv"
+
         try:
-            with open(filename, "r", newline="") as csvfile:
+            with open(filename, encoding="utf-8") as myfile:
+                r = csv.reader(myfile)
                 if cls.__name__ == "Rectangle":
-                    fieldnames = ["id", "width", "height", "x", "y"]
-                else:
-                    fieldnames = ["id", "size", "x", "y"]
-                list_dicts = csv.DictReader(csvfile, fieldnames=fieldnames)
-                list_dicts = [dict([k, int(v)] for k, v in d.items())
-                              for d in list_dicts]
-                return [cls.create(**d) for d in list_dicts]
+                    attr = ["id", "width", "height", "x", "y"]
+                elif cls.__name__ == "Square":
+                    attr = ["id", "size", "x", "y"]
+                inslist = []
+                for row in r:
+                    ct, dic = 0, {}
+                    for i in row:
+                        dic[attr[ct]] = int(i)
+                        ct += 1
+                    inslist.append(cls.create(**dic))
+                return inslist
         except IOError:
             return []
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        """ Draws Rectangles and Squares in GUI """
+
+        if (not list_rectangles and not list_squares) or \
+           (len(list_rectangles) == 0 and len(list_squares) == 0):
+            return
+
+        def drawfig(x, y, widthfig, heightfig, xoffset, yoffset, color,
+                    dd,  gr=turtle.Turtle()):
+            # Dot Distance
+            dd = dd
+            # Dot Size
+            dz = 2
+            # Angle 90 so it goes to next row
+            mov = 90
+
+            gr.goto(xoffset, yoffset)
+
+            # Y Movement
+
+            width = 1
+            height = y
+            for i in range(height):
+                for j in range(width):
+                    gr.dot(dz, "black")
+                    gr.forward(dd)
+                gr.backward(dd * width)
+                gr.right(mov)
+                gr.forward(dd)
+                gr.left(mov)
+
+            gr.goto(xoffset, -1 * y * dd + yoffset)
+
+            # X Movement
+
+            width = x
+            height = 1
+            for i in range(height):
+                for j in range(width):
+                    gr.dot(dz, "black")
+                    gr.forward(dd)
+                gr.backward(dd * width)
+                gr.right(mov)
+                gr.forward(dd)
+                gr.left(mov)
+
+            gr.goto(x * dd + xoffset, -1 * y * dd + yoffset)
+
+            # Draw Figure
+
+            width = widthfig
+            height = heightfig
+            for y in range(height):
+                for i in range(width):
+                    gr.dot(dz, color)
+                    gr.forward(dd)
+                gr.backward(dd * width)
+                gr.right(mov)
+                gr.forward(dd)
+                gr.left(mov)
+
+        # MAIN =========================================================
+        gr = turtle.Turtle()
+
+        # Dont show pen when drawing
+        gr.penup()
+        gr.pen(shown=False)
+
+        # Animation Speed
+        # gr.speed(0)
+
+        # Disable Animation
+        # turtle.setup(2000, 2000)
+        turtle.tracer(False)
+
+        colors = ["blue", "green", "red", "purple",
+                  "brown", "orange", "gold"]
+        cl = 0
+        # Distance between dots
+        dd = 3
+        # Space between figs
+        space = 10
+        # Offset movement
+        xoffset = -950
+        yoffset = 450
+        width, height, x, y = 0, 0, 0, 0
+        for r in list_rectangles:
+            xoffset += (x + width) * dd + space
+            width, height = r.width, r.height
+            x, y = r.x, r.y
+            drawfig(x, y, width, height, xoffset, yoffset, colors[cl], dd, gr)
+            cl = cl + 1 if cl < 5 else 0
+
+        yoffset = 0
+        ct = 0
+        for s in list_squares:
+            if ct == 0:
+                xoffset = (dd + space) - 950
+                ct = 1
+            else:
+                xoffset += (x + width) * dd + space
+            width, height = s.size, s.size
+            x, y = s.x, s.y
+            drawfig(x, y, width, height, xoffset, yoffset, colors[cl], dd, gr)
+            cl = cl + 1 if cl < 5 else 0
+
+        turtle.done()
