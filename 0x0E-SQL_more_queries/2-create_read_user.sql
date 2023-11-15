1b11b27f4dc9cfb7481d51070c5f3da1f390abdd
TheSoftNode <thesoftnode@gmail.com>
2023-11-15 15:06:01 -0800

    Added some tasks

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,7 @@
--- Creates the database hbtn_0d_2 and the user user_0d_2
--- Query to perform operation
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost'
-      IDENTIFIED BY 'user_0d_2_pwd';
+-- create databse  and user set pass word and others
+CREATE DATABASE IF NOT EXISTS`hbtn_0d_2`;
+CREATE USER
+    IF NOT EXISTS 'user_0d_2'@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT
+    ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
